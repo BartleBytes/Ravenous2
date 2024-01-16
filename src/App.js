c90862a70d841ed47ca0224f4fc4259a1793ed88
dylanbartle <dylanbartle@hotmail.com>
2024-01-16 16:57:30 -0700

    new commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./App.module.css";
 
-import BusinessList from './components/BusinessList'
-import SearchBar from './components/SearchBar'
+import BusinessList from "./components/BusinessList";
+import SearchBar from "./components/SearchBar";
+import Yelp from "./util/Yelp";
 
 const App = () => {
+  const [businesses, setBusinesses] = useState([]);
+
+  const searchYelp = async (term, location, sortBy) => {
+    const businesses = await Yelp.search(term, location, sortBy);
+    setBusinesses(businesses);
+  };
+
   return (
     <div className={styles.App}>
       <h1>ravenous</h1>
-      <SearchBar />
-      <BusinessList />
+      <SearchBar searchYelp={searchYelp} />
+      <BusinessList businesses={businesses} />
     </div>
   );
 };
